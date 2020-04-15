2b7d9fb0a18c841b3bc1744272517f5ddd595907
codeflow <1149863953@qq.com>
2020-04-15 14:44:30 +0800

    添加podspec

---
+++ CommonKit/CommonKit/Sources/Extensions/UIView+Extention.swift
@@ -0,0 +1,210 @@
+//
+//  UIView+Extention.swift
+//  znpx
+//
+//  Created by CaoWen on 2018/8/9.
+//  Copyright © 2018年 wawi. All rights reserved.
+//
+
+import UIKit
+
+let ScreenWidth = UIScreen.main.bounds.width
+let ScreenHeight = UIScreen.main.bounds.height
+let StatusBarHeight = UIApplication.shared.statusBarFrame.size.height
+let iPhone = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.phone)
+let iPhoneX = (ScreenHeight >= 812.0 && ScreenWidth >= 375.0 && iPhone)
+let tabBarHeight = (CGFloat)(iPhoneX ? (49.0 + 34.0) : (49.0))
+
+extension UIView {
+    
+    // MARK: @IBInspectable
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable var masksToBounds: Bool {
+        get {
+            return layer.masksToBounds
+        }
+        
+        set {
+            layer.masksToBounds = newValue
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue > 0 ? newValue : 0
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue > 0 ? newValue : 0
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.shadowColor!)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue > 0 ? newValue : 0
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    // MARK: UIView.frame.origin
+    var cc_x: CGFloat {
+        get {
+            return self.frame.origin.x
+        }
+        set {
+            var frame = self.frame
+            frame.origin.x = newValue
+            self.frame = frame
+        }
+    }
+    
+    var cc_y: CGFloat {
+        get {
+            return self.frame.origin.y
+        }
+        set {
+            var frame = self.frame
+            frame.origin.y = newValue
+            self.frame = frame
+        }
+    }
+}
+
+// Alerts and Pickers
+extension UIView {
+    func searchVisualEffectsSubview() -> UIVisualEffectView? {
+        if let visualEffectView = self as? UIVisualEffectView {
+            return visualEffectView
+        } else {
+            for subview in subviews {
+                if let found = subview.searchVisualEffectsSubview() {
+                    return found
+                }
+            }
+        }
+        return nil
+    }
+    
+    /// This is the function to get subViews of a view of a particular type
+    /// https://stackoverflow.com/a/45297466/5321670
+    func subViews<T : UIView>(type : T.Type) -> [T]{
+        var all = [T]()
+        for view in self.subviews {
+            if let aView = view as? T{
+                all.append(aView)
+            }
+        }
+        return all
+    }
+    
+    
+    /// This is a function to get subViews of a particular type from view recursively. It would look recursively in all subviews and return back the subviews of the type T
+    /// https://stackoverflow.com/a/45297466/5321670
+    func allSubViewsOf<T : UIView>(type : T.Type) -> [T]{
+        var all = [T]()
+        func getSubview(view: UIView) {
+            if let aView = view as? T{
+                all.append(aView)
+            }
+            guard view.subviews.count>0 else { return }
+            view.subviews.forEach{ getSubview(view: $0) }
+        }
+        getSubview(view: self)
+        return all
+    }
+}
+
+extension UIView {
+    func constrainToEdges(_ subview: UIView) {
+      
+      subview.translatesAutoresizingMaskIntoConstraints = false
+      
+      let topContraint = NSLayoutConstraint(
+        item: subview,
+        attribute: .top,
+        relatedBy: .equal,
+        toItem: self,
+        attribute: .top,
+        multiplier: 1.0,
+        constant: 0)
+      
+      let bottomConstraint = NSLayoutConstraint(
+        item: subview,
+        attribute: .bottom,
+        relatedBy: .equal,
+        toItem: self,
+        attribute: .bottom,
+        multiplier: 1.0,
+        constant: 0)
+      
+      let leadingContraint = NSLayoutConstraint(
+        item: subview,
+        attribute: .leading,
+        relatedBy: .equal,
+        toItem: self,
+        attribute: .leading,
+        multiplier: 1.0,
+        constant: 0)
+      
+      let trailingContraint = NSLayoutConstraint(
+        item: subview,
+        attribute: .trailing,
+        relatedBy: .equal,
+        toItem: self,
+        attribute: .trailing,
+        multiplier: 1.0,
+        constant: 0)
+      
+      addConstraints([
+        topContraint,
+        bottomConstraint,
+        leadingContraint,
+        trailingContraint])
+    }
+}
